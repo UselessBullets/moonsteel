70c1171a5d05691e31c95eb0ab25d7406b717af8
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-02-22 16:19:05 -0600

    fall distance fix

--- src/main/java/useless/moonsteel/mixin/entity/EntityLivingMixin.java
+++ src/main/java/useless/moonsteel/mixin/entity/EntityLivingMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import useless.moonsteel.interfaces.IMoonGrav;
@@ -40,6 +41,13 @@ public abstract class EntityLivingMixin extends Entity {
 		} else {
 			entity.yd = yd;
 		}
+	}
+	@ModifyVariable(method = "causeFallDamage(F)V", at = @At(value = "STORE"), ordinal = 0)
+	private int changeFallDamage(int i){
+		if (this instanceof IMoonGrav){
+			return (int)((i * ((IMoonGrav) this).moonsteel$getGravScalar()) - (3/((IMoonGrav) this).moonsteel$getGravScalar()) + 3);
+		}
+		return i;
 	}
 
 	@Inject(method = "onDeath(Lnet/minecraft/core/entity/Entity;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/entity/EntityLiving;dropFewItems()V"))
--- src/main/java/useless/moonsteel/mixin/entity/EntityZombieArmoredMixin.java
+++ src/main/java/useless/moonsteel/mixin/entity/EntityZombieArmoredMixin.java
@@ -9,14 +9,13 @@ import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import useless.moonsteel.MoonSteel;
 import useless.moonsteel.interfaces.IMoonGrav;
 import useless.moonsteel.interfaces.IStarZombie;
-import useless.moonsteel.MoonSteel;
 
 @Mixin(value = EntityArmoredZombie.class, remap = false)
 public class EntityZombieArmoredMixin extends EntityZombie implements IMoonGrav, IStarZombie {
